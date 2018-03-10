11a505f7de4770619fae22a07149b27300b7be9f
Erik Hedvall <erikwhedvall@gmail.com>
2018-03-10 16:01:02 +0100

    Add pixel conversion tests with alpha channel

--- src/macros.rs
+++ src/macros.rs
@@ -24,30 +24,63 @@ macro_rules! raw_pixel_conversion_tests {
 
     (@float_array_test $float: ty, $name: ident <$($ty_param: ident),+> : $($component: ident),+) => {
         use ::Pixel;
+        use ::Alpha;
 
         let mut counter: $float = 0.0;
         $(
             counter += 0.1;
             let $component = counter;
         )+
+        let alpha = counter + 0.1;
+
         let raw: [$float; <$name<$($ty_param,)+ $float> as Pixel<$float>>::CHANNELS] = [$($component),+];
+        let raw_plus_1: [$float; <$name<$($ty_param,)+ $float> as Pixel<$float>>::CHANNELS + 1] = [
+            $($component,)+
+            alpha
+        ];
         let color: $name<$($ty_param,)+ $float> = *$name::from_raw(&raw);
+        let color_long: $name<$($ty_param,)+ $float> = *$name::from_raw(&raw_plus_1);
+
+        let color_alpha: Alpha<$name<$($ty_param,)+ $float>, $float> = *Alpha::<$name<$($ty_param,)+ $float>, $float>::from_raw(&raw_plus_1);
 
         assert_eq!(color, $name::new($($component),+));
+        assert_eq!(color_long, $name::new($($component),+));
+
+        assert_eq!(color_alpha, Alpha::<$name<$($ty_param,)+ $float>, $float>::new($($component,)+ alpha));
     };
 
     (@float_slice_test $float: ty, $name: ident <$($ty_param: ident),+> : $($component: ident),+) => {
         use ::Pixel;
+        use ::Alpha;
 
         let mut counter: $float = 0.0;
         $(
             counter += 0.1;
             let $component = counter;
         )+
+        let alpha = counter + 0.1;
+        let extra = counter + 0.2;
         let raw: &[$float] = &[$($component),+];
+        let raw_plus_1: &[$float] = &[
+            $($component,)+
+            alpha
+        ];
+        let raw_plus_2: &[$float] = &[
+            $($component,)+
+            alpha,
+            extra
+        ];
         let color: $name<$($ty_param,)+ $float> = *$name::from_raw(raw);
+        let color_long: $name<$($ty_param,)+ $float> = *$name::from_raw(raw_plus_1);
+
+        let color_alpha: Alpha<$name<$($ty_param,)+ $float>, $float> = *Alpha::<$name<$($ty_param,)+ $float>, $float>::from_raw(raw_plus_1);
+        let color_alpha_long: Alpha<$name<$($ty_param,)+ $float>, $float> = *Alpha::<$name<$($ty_param,)+ $float>, $float>::from_raw(raw_plus_2);
 
         assert_eq!(color, $name::new($($component),+));
+        assert_eq!(color_long, $name::new($($component),+));
+
+        assert_eq!(color_alpha, Alpha::<$name<$($ty_param,)+ $float>, $float>::new($($component,)+ alpha));
+        assert_eq!(color_alpha_long, Alpha::<$name<$($ty_param,)+ $float>, $float>::new($($component,)+ alpha));
     };
 }
 
