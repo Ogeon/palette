33bc4ff5a08eae2fe5cc5091c8b949780137b8f4
Erik Hedvall <erikwhedvall@gmail.com>
2020-04-19 12:23:24 +0200

    Use a feature flag to stop no_std_test from blocking builds

--- no_std_test/src/main.rs
+++ no_std_test/src/main.rs
@@ -1,10 +1,12 @@
-#![feature(lang_items, start)]
+#![cfg_attr(feature = "nightly", feature(lang_items, start))]
 #![no_std]
 
+#[cfg(feature = "nightly")]
 use core::panic::PanicInfo;
 
 extern crate libc;
 
+#[cfg(feature = "nightly")]
 #[start]
 fn start(_argc: isize, _argv: *const *const u8) -> isize {
     let _magenta = palette::Srgb::new(255u8, 0, 255);
@@ -12,10 +14,15 @@ fn start(_argc: isize, _argv: *const *const u8) -> isize {
     0
 }
 
+#[cfg(feature = "nightly")]
 #[lang = "eh_personality"]
 extern "C" fn eh_personality() {}
 
+#[cfg(feature = "nightly")]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
+#[cfg(not(feature = "nightly"))]
+fn main() {}
