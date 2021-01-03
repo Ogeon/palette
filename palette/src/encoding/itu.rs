b6e379453ce56d0c9a540b8c8e6b2c3dde960e03
Andreas Molzer <andreas.molzer@gmx.de>
2021-01-03 03:24:32 +0100

    Add values for BT.2020 rgb and yuv spaces

--- palette/src/encoding/itu.rs
+++ palette/src/encoding/itu.rs
@@ -35,9 +35,24 @@ pub struct BT601_625;
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct BT709;
 
+/// The color space of ITU-R BT2020.
+///
+/// See [ITU-R Rec.2020].
+///
+/// [ITU-R Rec.2020]: https://www.itu.int/rec/R-REC-BT.2020/
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct BT2020;
+
 /// This transfer function is shared between `BT601` and `BT709`.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Transfer601And709;
+
+/// The transfer function of `BT2020`.
+///
+/// This is technically very similar to the one of BT.601 and BT.709 but the constants are defined
+/// with increased accuracy due to supporting up to 12-bit quantized numbers.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct Transfer2020;
 
 /// The Yuv encoding difference functions for BT601.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -46,6 +61,10 @@ pub struct DifferenceFn601;
 /// The Yuv encoding difference functions for BT709.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct DifferenceFn709;
+
+/// The Yuv encoding difference functions for BT2020.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct DifferenceFn2020;
 
 // See 2.5.1 (page 2). RGB primary luminances.
 const BT601_LUMINANCE: (f64, f64, f64) = (0.2990, 0.5870, 0.1140);
@@ -63,6 +82,13 @@ const BT709_WEIGHTS: (f64, f64, f64) = (0.2126, 0.7152, 0.07212);
 const BT709_BLUE_NORM: f64 = 1.8556;
 // Divisor to renormalize the red difference signal.
 const BT709_RED_NORM: f64 = 1.5748;
+
+// See Table 4, Derivation of Luminance.
+const BT2020_WEIGHTS: (f64, f64, f64) = (0.2627, 0.6780, 0.0593);
+// Divisor to renormalize the blue difference.
+const BT2020_BLUE_NORM: f64 = 1.8814;
+// Divisor to renormalize the red difference.
+const BT2020_RED_NORM: f64 = 1.4746;
 
 impl Primaries for BT601_525 {
     fn red<Wp: WhitePoint, T: FloatComponent>() -> Yxy<Wp, T> {
@@ -100,6 +126,18 @@ impl Primaries for BT709 {
     }
 }
 
+impl Primaries for BT2020 {
+    fn red<Wp: WhitePoint, T: FloatComponent>() -> Yxy<Wp, T> {
+        Yxy::with_wp(cast(0.708), cast(0.292), cast(BT2020_WEIGHTS.0))
+    }
+    fn green<Wp: WhitePoint, T: FloatComponent>() -> Yxy<Wp, T> {
+        Yxy::with_wp(cast(0.170), cast(0.797), cast(BT2020_WEIGHTS.1))
+    }
+    fn blue<Wp: WhitePoint, T: FloatComponent>() -> Yxy<Wp, T> {
+        Yxy::with_wp(cast(0.131), cast(0.046), cast(BT2020_WEIGHTS.2))
+    }
+}
+
 impl RgbSpace for BT601_525 {
     type Primaries = BT601_525;
     type WhitePoint = D65;
@@ -112,6 +150,11 @@ impl RgbSpace for BT601_625 {
 
 impl RgbSpace for BT709 {
     type Primaries = BT709;
+    type WhitePoint = D65;
+}
+
+impl RgbSpace for BT2020 {
+    type Primaries = BT2020;
     type WhitePoint = D65;
 }
 
@@ -130,6 +173,11 @@ impl RgbStandard for BT709 {
     type TransferFn = Transfer601And709;
 }
 
+impl RgbStandard for BT2020 {
+    type Space = BT2020;
+    type TransferFn = Transfer2020;
+}
+
 impl LumaStandard for BT601_525 {
     type WhitePoint = D65;
     type TransferFn = Transfer601And709;
@@ -143,6 +191,11 @@ impl LumaStandard for BT601_625 {
 impl LumaStandard for BT709 {
     type WhitePoint = D65;
     type TransferFn = Transfer601And709;
+}
+
+impl LumaStandard for BT2020 {
+    type WhitePoint = D65;
+    type TransferFn = Transfer2020;
 }
 
 impl YuvStandard for BT601_525 {
@@ -163,6 +216,12 @@ impl YuvStandard for BT709 {
     type DifferenceFn = DifferenceFn709;
 }
 
+impl YuvStandard for BT2020 {
+    type RgbSpace = Self;
+    type TransferFn = Transfer2020;
+    type DifferenceFn = DifferenceFn2020;
+}
+
 impl TransferFn for Transfer601And709 {
     fn into_linear<T: Float + FromF64>(x: T) -> T {
         if x <= cast(0.0091) {
@@ -177,6 +236,30 @@ impl TransferFn for Transfer601And709 {
             x * cast(4.500)
         } else {
             x.powf(cast(0.45)) * cast(1.099) - cast(0.099)
+        }
+    }
+}
+
+impl TransferFn for Transfer2020 {
+    fn into_linear<T: Float + FromF64>(x: T) -> T {
+        let alpha: T = cast(1.09929682680944);
+        let beta: T = cast(0.018053968510807);
+
+        if x < beta * cast(4.500) {
+            x / cast(4.500)
+        } else {
+            ((x + alpha - T::one()) / alpha).powf(T::one() / cast(0.45))
+        }
+    }
+
+    fn from_linear<T: Float + FromF64>(x: T) -> T {
+        let alpha: T = cast(1.09929682680944);
+        let beta: T = cast(0.018053968510807);
+
+        if x < beta {
+            x * cast(4.5)
+        } else {
+            x.powf(cast(0.45)) * alpha - (alpha - T::one())
         }
     }
 }
@@ -226,5 +309,29 @@ impl DifferenceFn for DifferenceFn709 {
 
     fn denorm_red<T: FloatComponent>(norm: T) -> T {
         norm * cast(BT709_RED_NORM)
+    }
+}
+
+impl DifferenceFn for DifferenceFn2020 {
+    fn luminance<T: FloatComponent>() -> [T; 3] {
+        // Full intensity matches whitepoint, these are exactly the Y component of primares.
+        let (r, g, b) = BT2020_WEIGHTS;
+        [cast(r), cast(g), cast(b)]
+    }
+
+    fn norm_blue<T: FloatComponent>(denorm: T) -> T {
+        denorm / cast(BT2020_BLUE_NORM)
+    }
+
+    fn denorm_blue<T: FloatComponent>(norm: T) -> T {
+        norm * cast(BT2020_BLUE_NORM)
+    }
+
+    fn norm_red<T: FloatComponent>(denorm: T) -> T {
+        denorm / cast(BT2020_RED_NORM)
+    }
+
+    fn denorm_red<T: FloatComponent>(norm: T) -> T {
+        norm * cast(BT2020_RED_NORM)
     }
 }
