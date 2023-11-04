ceb19cdb887316be3d9ceec5917a12265b532bb0
Erik Hedvall <erikwhedvall@gmail.com>
2023-11-04 16:38:42 +0100

    Only import random distributions when the random feature is enabled

--- palette/src/cam16.rs
+++ palette/src/cam16.rs
@@ -11,8 +11,13 @@ use crate::{
 pub use full::*;
 pub use parameters::*;
 pub use partial::*;
-pub use ucs_jab::{Cam16UcsJab, Cam16UcsJaba, Iter as Cam16UcsJabIter, UniformCam16UcsJab};
-pub use ucs_jmh::{Cam16UcsJmh, Cam16UcsJmha, Iter as Cam16UcsJmhIter, UniformCam16UcsJmh};
+pub use ucs_jab::{Cam16UcsJab, Cam16UcsJaba, Iter as Cam16UcsJabIter};
+pub use ucs_jmh::{Cam16UcsJmh, Cam16UcsJmha, Iter as Cam16UcsJmhIter};
+
+#[cfg(feature = "random")]
+pub use ucs_jab::UniformCam16UcsJab;
+#[cfg(feature = "random")]
+pub use ucs_jmh::UniformCam16UcsJmh;
 
 mod full;
 mod math;
