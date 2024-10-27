0d6dcab26f140cbc72ffc433265afe48914f2372
Ogeon <Ogeon@users.noreply.github.com>
2024-10-27 10:58:24 +0000

    Deploying to gh-pages from @ Ogeon/palette@4a30161a542c5ccf7750cf36e38035213492a532 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["palette"];
+//{"start":21,"fragment_lengths":[9]}
--- trait.impl/palette/trait.Darken.js
+++ trait.impl/palette/trait.Darken.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"palette":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["palette",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[14]}
