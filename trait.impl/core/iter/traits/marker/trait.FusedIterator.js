b35f905851efdbb536a23229bf6942012d577c13
Ogeon <Ogeon@users.noreply.github.com>
2024-11-16 16:41:14 +0000

    Deploying to gh-pages from @ Ogeon/palette@8e22189f3cedac01f9f93b0d5f20ab2233d189ea 🚀

---
+++ trait.impl/core/iter/traits/marker/trait.FusedIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["palette",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Colors.html\" title=\"struct palette::named::Colors\">Colors</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Entries.html\" title=\"struct palette::named::Entries\">Entries</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Names.html\" title=\"struct palette::named::Names\">Names</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[949]}
