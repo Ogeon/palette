19eb3d8d54b9e1b85a59290804c0be6fa424a9eb
Ogeon <Ogeon@users.noreply.github.com>
2026-03-10 19:34:26 +0000

    Deploying to gh-pages from @ Ogeon/palette@3d7f410b546d59e812e55e6d1a7a08c93765e3f5 🚀

--- trait.impl/core/iter/traits/marker/trait.FusedIterator.js
+++ trait.impl/core/iter/traits/marker/trait.FusedIterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["palette",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Colors.html\" title=\"struct palette::named::Colors\">Colors</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Entries.html\" title=\"struct palette::named::Entries\">Entries</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Names.html\" title=\"struct palette::named::Names\">Names</a>"]]]]);
+    const implementors = Object.fromEntries([["palette",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Colors.html\" title=\"struct palette::named::Colors\">Colors</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Entries.html\" title=\"struct palette::named::Entries\">Entries</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"palette/named/struct.Names.html\" title=\"struct palette::named::Names\">Names</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[949]}
+//{"start":59,"fragment_lengths":[955]}
--- trait.impl/palette/cast/as_components_traits/trait.ComponentsAs.js
+++ trait.impl/palette/cast/as_components_traits/trait.ComponentsAs.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["palette",[]]]);
+    const implementors = Object.fromEntries([["palette",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[14]}
+//{"start":59,"fragment_lengths":[14]}
